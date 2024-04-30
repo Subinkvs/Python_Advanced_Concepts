a2e5aca3e081b4938fddd7e03d3492aaac65e7a0
SubinSundar <subinsundar9624@gmail.com>
2024-04-30 17:51:09 +0530

    successfully done a problem in dictionary

--- new_python.py
+++ new_python.py
@@ -8,6 +8,16 @@ dic2 = {
      "age": 10
 }
 
+dic7 = {
+     "name": "nima",
+     "age": 10
+}
+
+dic8 = {
+     "name": "nami",
+     "age": 10
+}
+
 dic3 = {
     "name": "Sanju",
     "age": 29
@@ -19,18 +29,70 @@ dic4 = {
 
 }
 
+dic5 = {
+    "name":"Sohu",
+    "age": 24
 
-list = [dic1, dic2, dic3, dic4]
+}
 
-for i in list:
-    if i['name'][0] == 'S' and i['age'] > 28:
-        print(i)
+dic6 = {
+    "name":"Sworav",
+    "age": 24
 
-# print(list)
+}
 
-# def SearchName(list, char):
+list = [dic1, dic2, dic3, dic4, dic5, dic6, dic7, dic8]
+
+
+
+def SearchName(list, char):
+    
+    count = 0
+    
+    for item in list:           
+        if item['name'][0] == char:
+            count = count + 1
+            if count < 3:
+                print(item)
+            
+        
+
+        
+SearchName(list, 'n')
+
+# def test_tuple():
+    
+#     list = [11, 22, 33, 44, 55, 66, 220]
 #     for i in list:
-#         print(i.name == char)
+#         print(i)
+    
+#     tuple = (1, 2, 3, 4, 5, 6)
+#     for i in tuple:
+#         print(i)
+        
+# test_tuple()
+# import sys
 
-# SearchName(list, 'S')
+# list = [1, 2, 3, 4, 5, 6]
+# tuple = (1, 2, 3, 4, 5, 8, 9)
 
+# print(sys.getsizeof(list))
+# print(sys.getsizeof(tuple))
+
+# tuple = ('Subin', 28, 'Software Engineer')
+
+# name, age, profession = tuple
+
+# print("Name:", name, "Age:", age, "Profession:", profession)
+
+# name, *rest = tuple
+
+# print(rest)
+
+
+# first, *second = list
+
+# print(second)
+
+# name = dic1["name"]
+# print(name)
